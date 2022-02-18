2ba21e141961768dd77e367cdd8116e3e70b3009
Alex Taylor <alexhenrytaylor@gmail.com>
2022-02-18 11:51:28 +0000

    fixing bundler

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 module RailsWatchList
   class Application < Rails::Application
